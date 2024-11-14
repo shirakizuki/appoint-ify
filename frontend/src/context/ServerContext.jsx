499e7512cf39d3087dca9ef0ea6b274b9756ea47
Charles Tigoy <135233171+lomawezila@users.noreply.github.com>
2024-11-14 17:56:30 +0800

    Updated listening port

--- frontend/src/context/ServerContext.jsx
+++ frontend/src/context/ServerContext.jsx
@@ -5,7 +5,7 @@ export const ServerContext = createContext()
 
 const ServerContextProvider = (props) => {
     // BASE API URL
-    const url = "http://localhost:4000/api";
+    const url = "https://appoint-ify-bw1r.vercel.app/api";
     // BASE API TOKEN
     const [token, setToken] = useState('');
     // BASE CONTEXT VALUE

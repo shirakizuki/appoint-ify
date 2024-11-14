513461ec2bddd4534e7686a8cbe70f881f75710d
Charles Tigoy <135233171+lomawezila@users.noreply.github.com>
2024-11-14 18:23:52 +0800

    Kapoy nako

--- frontend/src/context/ServerContext.jsx
+++ frontend/src/context/ServerContext.jsx
@@ -5,7 +5,7 @@ export const ServerContext = createContext()
 
 const ServerContextProvider = (props) => {
     // BASE API URL
-    const url = "localhost:4000/api";
+    const url = "http://localhost:4000/api";
     // BASE API TOKEN
     const [token, setToken] = useState('');
     // BASE CONTEXT VALUE

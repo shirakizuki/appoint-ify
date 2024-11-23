a117dd3461c6c26cf8fbafd94f5fdf1f714a26aa
Charles Tigoy <135233171+lomawezila@users.noreply.github.com>
2024-11-24 04:21:28 +0800

    Updated url.

--- clients/src/context/ServerContext.jsx
+++ clients/src/context/ServerContext.jsx
@@ -5,7 +5,7 @@ export const ServerContext = createContext()
 
 const ServerContextProvider = (props) => {
     // BASE API URL
-    const url = "http://localhost:4000/api";
+    const url = "https://appoint-ify.onrender.com/";
     // BASE API TOKEN
     const [token, setToken] = useState('');
     // BASE CONTEXT VALUE
